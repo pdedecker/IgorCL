8b63d8af9947d61e7c8496af502e9483a33987a5
Peter Dedecker <peter.dedecker@hotmail.com>
2013-02-06 11:22:57 +0100

    Added some more files that were missing

---
+++ IgorCL.r
@@ -0,0 +1,43 @@
+#include "XOPStandardHeaders.r"
+
+resource 'vers' (1) {						// XOP version info.
+	0x01, 0x20, release, 0x00, 0,			// version bytes and country integer.
+	"1.20",
+	"1.20, Copyright 1993-2010 WaveMetrics, Inc., all rights reserved."
+};
+
+resource 'vers' (2) {						// Igor version info.
+	0x05, 0x00, release, 0x00, 0,			// Version bytes and country integer.
+	"5.00",
+	"(for Igor 5.00 or later)"
+};
+
+// Custom error messages
+resource 'STR#' (1100) {
+	{
+		/* [1] */
+		"IgorCL requires Igor Pro 6.0 or later.",
+	}
+};
+
+// No menu item
+
+resource 'XOPI' (1100) {
+	XOP_VERSION,							// XOP protocol version.
+	DEV_SYS_CODE,							// Development system information.
+	0,										// Obsolete - set to zero.
+	0,										// Obsolete - set to zero.
+	XOP_TOOLKIT_VERSION,					// XOP Toolkit version.
+};
+
+resource 'XOPC' (1100) {
+	{
+		"IgorCL",								// Name of operation.
+		waveOP+XOPOp+compilableOp,			// Operation's category.
+        
+        "IGORCLInfo",								// Name of operation.
+		waveOP+XOPOp+compilableOp,			// Operation's category.
+	}
+};
+
+
